cb9a2c7e2694465d924d5753b3c55cca2ea550da
fatmatasdemir <105522049+fatmatasdemir@users.noreply.github.com>
2022-08-19 13:54:40 +0200

    Update C03.java

--- src/C03.java
+++ src/C03.java
@@ -2,6 +2,8 @@ public class C03 {
 
     public static void main(String[] args) {
         System.out.println("Fatma hoca ile github ogrenmek cok zevkli");
+         System.out.println("Github'dan Locale gonderme");
+        
 
     }
 
