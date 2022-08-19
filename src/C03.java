2923fcc108afcacf9f7dc80746f62365c566283e
Fatma Tasdemir <f.tasdemir571@gmail.com>
2022-08-19 14:00:07 +0200

    M1

--- src/C03.java
+++ src/C03.java
@@ -3,6 +3,7 @@ public class C03 {
     public static void main(String[] args) {
         System.out.println("Fatma hoca ile github ogrenmek cok zevkli");
          System.out.println("Github'dan Locale gonderme");
+        System.out.println("Master 1.Deneme");
         
 
     }
